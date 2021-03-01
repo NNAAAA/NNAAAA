4f48f3114c4eb5cbacd95ac2a1d6e16ba53dc7f1
NNAAAA <nngguyen.anh@gmail.com>
2021-03-01 21:26:22 +0700

    update lucky-numb c++

--- assignment/lucky-numb/cpp-text/index.cpp
+++ assignment/lucky-numb/cpp-text/index.cpp
@@ -2,106 +2,77 @@
 #include <fstream>
 using namespace std;
 
-// void push(int *arr,int &length,int x){
-//   length++;
-//   int *clone=new int[length];
-//   for (int i=0;i<length;++i){
-//     clone[i]=arr[i];
-//   }
-//   delete[] arr;
-//   clone[length-1]=x;
-//   arr=clone;
-  
-// }
 
-void loadFile(ifstream &f,int *arr,int &length){
-  while(!f.eof()){
-    f>>arr[length];
-    length++;
-  }
-  length--;
-  f.close();
+void createArr(int arrRequire[][30],int &currentIndex,int &length){
+ 	while(currentIndex<=1000){
+		
+	    int *clone=new int[length];
+	      
+	    //khoi tao day so va luu 3...3
+	    for (int i=0;i<length;++i){
+	      clone[i]=3;  
+	    }
+	    for (int i=0;i<length;++i){
+    		arrRequire[currentIndex][i]=clone[i];
+  		}
+	    currentIndex++;
+	
+	
+		//chay tu cuoi len
+	    int currentPos=length-1;
+		int currentNumb=3;
+	    while(true){	    	
+		    for (int j=length-1;j>=((currentPos>=0) ? currentPos : 0);--j){
+		        while(clone[j]<7){
+			        clone[j]+=2;
+			        //luu clone vao mang chinh
+			        for (int i=0;i<length;++i){
+    					arrRequire[currentIndex][i]=clone[i];
+  					}
+			        currentIndex++; 
+		        }
+		    }
+		    if (clone[0]==7){
+		    	length++;
+			    break;
+			}
+
+		
+		    currentPos--;
+		    //keo gia tri xuong 
+			for (int j=((currentPos>=0) ? currentPos : 0);j<length;++j){
+			    clone[j]=currentNumb+2;	
+			}
+			
+			//luu clone vao mang chinh
+			for (int i=0;i<length;++i){
+    			arrRequire[currentIndex][i]=clone[i];
+  			}
+			currentIndex++; 		    		
+    	} 
+		delete [] clone;	
+  	}
 }
 
-
-
-void saveFile(ofstream &f,int *arr,int &length){
-  for (int i=0;i<length-1;++i){
-    
-    f<<arr[i]<<endl;
-  }
-  f.close();
-}
-
-
-
-void require(int arr[1000]){
-  int currentIndex=0;
-  int currentNumb=3;
-  int length=1;
-  while(currentIndex<1000){
-    // if (currentNumb%3==0||currentNumb%5==0||currentNumb%7==0){
-    //   int clone=currentNumb,i=3;
-    //   while(i<=7){
-    //     if (clone%i==0){
-    //       clone/=i;
-    //     }
-    //     else
-    //       i+=2;
-    //   }
-    //   if (clone==1){
-    //     cout<<currentIndex+1<<":"<<currentNumb<<endl;
-    //     arr[currentIndex]=currentNumb;
-    //     currentIndex++;
-    //   }
-    // }
-    // currentNumb++;
-    int *clone=new int[length];
-    if (clone[0]==7)
-      length++;
-
-    for (int i=0;i<length;++i){
-      clone[i]=3;
-    }
-    currentIndex++;
-
-    int currentPos=length-1;
-    for (int i=0;i<length;++i){
-      if (clone[currentPos]==7)
-        currentPos++;
-      for (int j=currentPos;i<length;++j){
-        while()
-      }
-    }
-  }
+void printArr(int arrRequire[][30],int currentIndex){
+  	for (int i=0;i<currentIndex;++i){
+  		cout<<"index:"<<i<<"=";
+	    for (int j=0;j<30;++j){
+	        cout<<arrRequire[i][j]<<" ";
+	    }
+	    cout<<endl;
+  	}
 }
 
 int main()
 {
-  ifstream f;
-  int length=0;
-  int *arr=new int[10];
-  f.open("./text-folder/input.txt");
-  
-  if (!f.is_open())
-    cout<<"khong mo duoc file";
-  else {
-    loadFile(f,arr,length);
-  }
+  	int arrRequire[10000][30];
+  	int currentIndex=0;
+  	int length=1;
 
-
-  int arrRequire[1000];
-  require(arrRequire);
-
-
-
-  ofstream o;
-  o.open("./text-folder/output.txt");
-  if (!o.is_open())
-    cout<<"khong mo duoc file";
-  else {
-    saveFile(o,arr,length);
-  }
-  system("pause");
-  return 0;
+	createArr(arrRequire,currentIndex,length);
+	printArr(arrRequire,currentIndex);
+  	
+    system("pause");
+    return 0;
 }
