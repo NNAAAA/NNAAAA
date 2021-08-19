e7e6d45edba5d5aef1db63d42b519080543ea95d
NNAAAA <nngguyen.anh@gmail.com>
2021-08-19 20:38:02 +0700

    nnaaa's commit ^^

--- Nodejs/testing/src/views/header.js
+++
@@ -1,6 +0,0 @@
-const link=document.querySelectorAll(".nav-link");
-link.map(i=>{
-  i.addEventListener("click",e=>{
-    console.log(e)
-  })
-})
--- Reactjs/steam/BackEnd/src/route/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const userRoute=express.Router()
-const UserController=require('../controller/user.controller')
-
-userRoute.post('/register',UserController.postRegister)
-
-module.exports=userRoute
